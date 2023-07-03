8d70088246ef8c927b2ba67922d201ea1b441f35
sweing <sjweingaertner@gmail.com>
2023-07-04 00:02:01 +0200

    removing old files

--- backend-mastodon/register_app.py
+++
@@ -1,7 +0,0 @@
-from mastodon import Mastodon
-
-Mastodon.create_app(
-    'social-networks-live-map',
-    api_base_url = 'https://mastodon.social',
-    to_file = 'backend_mastodon_clientcred.secret'
-)
