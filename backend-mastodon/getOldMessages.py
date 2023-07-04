955e359d1ec98cd2dfa88883eb4ca06561070887
sweing <sjweingaertner@gmail.com>
2023-07-04 10:39:03 +0200

    move messages to archive if not found

--- backend-mastodon/getOldMessages.py
+++ backend-mastodon/getOldMessages.py
@@ -2,12 +2,21 @@ from mastodon import Mastodon
 import json
 from datetime import datetime
 import os
+import shutil
 
 # Define the URL you want to check for in the messages
 target_url = 'map.decarbnow.space'
 
-# Define the path for the data folder
+# Define the path for the data folder and archive subfolder
 data_folder = "data"
+archive_folder = os.path.join(data_folder, "archive")
+
+# Ensure the data and archive folders exist
+os.makedirs(data_folder, exist_ok=True)
+os.makedirs(archive_folder, exist_ok=True)
+
+# Get a list of existing message IDs in the data folder
+existing_ids = [filename.split(".")[0] for filename in os.listdir(data_folder) if filename.endswith(".json")]
 
 # Create an instance of Mastodon
 mastodon = Mastodon(access_token='backend_mastodon_usercred.secret')
@@ -21,12 +30,11 @@ class DateTimeEncoder(json.JSONEncoder):
 
 # Search for messages containing the target URL
 search_results = mastodon.search(target_url)
-print(search_results)
 
 # Check each search result for the target URL
 for status in search_results['statuses']:
     # Generate a unique filename using ID
-    message_id = status['id']
+    message_id = str(status['id'])
     filename = f'{message_id}.json'
     filepath = os.path.join(data_folder, filename)
 
@@ -35,3 +43,15 @@ for status in search_results['statuses']:
         json.dump(status, output_file, cls=DateTimeEncoder)
 
     print(f'Message with ID {message_id} saved to {filepath}')
+
+    # Remove the ID from the existing IDs list if found
+    if message_id in existing_ids:
+        existing_ids.remove(message_id)
+
+# Move any remaining files in the data folder to the archive folder
+for message_id in existing_ids:
+    filename = f'{message_id}.json'
+    filepath = os.path.join(data_folder, filename)
+    archive_filepath = os.path.join(archive_folder, filename)
+    shutil.move(filepath, archive_filepath)
+    print(f'Message with ID {message_id} moved to archive folder')
