eb495453af67ba34c2a2ff652ef8319afe5d4f08
sweing <sjweingaertner@gmail.com>
2023-07-08 23:42:36 +0200

    removing login.py

--- backend-mastodon/login.py
+++
@@ -1,8 +0,0 @@
-from mastodon import Mastodon
-
-mastodon = Mastodon(client_id = 'backend_mastodon_clientcred.secret',)
-mastodon.log_in(
-    'your@email',
-    'password',
-    to_file = 'backend_mastodon_usercred.secret'
-)
